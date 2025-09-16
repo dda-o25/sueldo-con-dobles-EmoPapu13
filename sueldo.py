a662f3e3d135ca867d23fba86cb78f48b748539b
EmoPapu13 <futemo1307@gmail.com>
2025-09-15 23:03:21 -0600

    primer_intento

--- sueldo.py
+++ sueldo.py
@@ -1,16 +1,29 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+El programa calcula el sueldo mensual semanal de un empleado. Si se trabaja mas de 48 hotas, las horas 
+extras se pagan al doble de la tarifa normal.
 """
 
 
 # Declaraciones
-CONSTANTE = valor
+
 
 # Entradas
-entrada = input()
+horas_trabajadas=float(input())
+tarifa=float(input())
 
 # Proceso
+if horas_trabajadas > 48:
+    horas_extras = horas_trabajadas - 48
+    pago_extra = horas_extras * tarifa * 2
+    pago = 48 * tarifa
+else:
+    horas_extras = 0
+    pago_extra = 0
+    pago = horas_trabajadas * tarifa
 
+total_a_pagar = pago + pago_extra
 
 # Salidas
-print(salida)
+print(horas_extras)
+print(pago_extra)
+print(total_a_pagar)
